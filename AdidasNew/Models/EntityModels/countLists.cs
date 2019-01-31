445f06d4b48f5fa10d88b9ac0b6e45995c362a54
mansour <smm.2020@gmail.com>
2019-02-01 00:55:58 +0330

    2

--- AdidasNew/Models/EntityModels/countLists.cs
+++ AdidasNew/Models/EntityModels/countLists.cs
@@ -10,6 +10,11 @@ namespace AdidasNew.Models.EntityModels
         public int NewPerson { get; set; }
         public int ConfirmedPerson { get; set; }
         public int NotConfirmedPerson { get; set; }
+
+        public int NewPersonUncheck { get; set; }
+        public int ConfirmedPersonUncheck { get; set; }
+        public int NotConfirmedPersonUncheck { get; set; }
+
         AdidasNew.Models.DomainModels.DatabaseContext db = null;
         public countLists()
         {
@@ -17,6 +22,8 @@ namespace AdidasNew.Models.EntityModels
             ConfirmedPerson = db.People.Where(p => p.Checked == true & p.Status == 2).ToList().Count();
             NotConfirmedPerson = db.People.Where(p => p.Checked == true & p.Status == 1).ToList().Count();
             NewPerson = db.People.Where(p => p.Checked == false ).ToList().Count();
+
+
 
         }
 
