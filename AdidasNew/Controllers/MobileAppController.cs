54636f3ba6b753d3308b4578fd3e14e4b0ced5b1
mansour <smm.2020@gmail.com>
2019-02-10 13:13:08 +0330

    start to question app

---
+++ AdidasNew/Controllers/MobileAppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdidasNew.Controllers
+{
+    public class MobileAppController : Controller
+    {
+        // GET: MobileApp
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
